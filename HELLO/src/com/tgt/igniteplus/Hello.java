00305b6a92917249086c44a9c9eefe6100676ad8
pragathiindran <50520367+pragathiindran@users.noreply.github.com>
2020-05-27 20:31:58 +0530

    Add files via upload

---
+++ HELLO/src/com/tgt/igniteplus/Hello.java
@@ -0,0 +1,10 @@
+/*A simple program to print "HELLO WORLD"*/
+package com.tgt.igniteplus;
+public class Hello {
+    public static void main(String[] args){
+        System.out.print("HELLO WORLD");
+    }
+}
+/* OUTPUT:
+HELLO WORLD
+ */
